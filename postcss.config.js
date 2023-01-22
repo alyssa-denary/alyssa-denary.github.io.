4984c9706063c605bce1099e525d5c61d8920199
Alyssa Denary <alyssa.denary@gmail.com>
2023-01-22 13:37:05 -0500

    add: postcss config

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("postcss-preset-env")({ stage: 1 }), // Allows use of future CSS
+    require("postcss-nested"),
+    require("postcss-custom-media"),
+    require("postcss-media-minmax"),
+    // require("cssnano"), // Minimizes CSS code to fewer lines/ removes line breaks
+  ],
+};
